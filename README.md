9c1ba05a4a1c88501a1b13685cc046b604bd2a24
Louisa Shi <louisa_shi@126.com>
2024-03-26 15:06:00 -0700

    update app.py

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TECHIN510-lab1
 Repository for MSTI course 510 lab1
+
+## Overview
+Profile page
+
+## Run
+```
+streamlit.py run app.py
+```
--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Ian Chen - Engineer, Educator, Entrepreneur",
-    page_icon="👨🏻‍💻",
+    page_title="Louisa Shi - Student at University of Washington",
+    page_icon="",
     layout="centered",  # centered or wide
     initial_sidebar_state="auto",
 )
