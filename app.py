bae8e7c8d1011e242d2da739d51372d2117fb45b
Louisa Shi <louisa_shi@126.com>
2024-03-26 15:56:35 -0700

    update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Louisa Shi - Student at University of Washington",
-    page_icon="",
+    page_icon="😎",
     layout="centered",  # centered or wide
     initial_sidebar_state="auto",
 )
